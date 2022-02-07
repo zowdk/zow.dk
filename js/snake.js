d9e4201a673db5805c4692acb481f8fe1ffacf57
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2022-02-07 11:02:42 +0100

    snake move function

--- js/snake.js
+++ js/snake.js
@@ -3,6 +3,7 @@ const startBtn = document.getElementById("start");
 const score = document.getElementById("score");
 let squares = [];
 let currentSnake = [0, 1, 2];
+let direction = 1;
 
 function createGrid() {
   //create 100 of these elements with a for loop
@@ -20,3 +21,15 @@ function createGrid() {
 createGrid();
 
 currentSnake.forEach((index) => squares[index].classList.add("snake"));
+
+function move() {
+  //remove last element from current snake array
+  const tail = currentSnake.pop();
+  //remove styling from last element
+  squares[tail].classList.remove("snake");
+  //add square in direction we are moving
+  currentSnake.unshift(currentSnake[0] + directrion);
+  //add styling so we can see movement
+  squares[currentSnake[0]].classList.add("snake");
+}
+move();

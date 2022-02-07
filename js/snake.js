1ddcef155f7158feb1e5c8916f68d91712f49c76
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2022-02-07 10:49:10 +0100

    create snake grid

---
+++ js/snake.js
@@ -0,0 +1,19 @@
+const grid = document.querySelector(".grid");
+const startBtn = document.getElementById("start");
+const score = document.getElementById("score");
+let squares = [];
+
+function createGrid() {
+  //create 100 of these elements with a for loop
+  for (i = 0; i < 100; i++) {
+    //create elements
+    const square = document.createElement("div");
+    //add styling to the element
+    square.classList.add("square");
+    //put the element into grid
+    grid.appendChild(square);
+    //push it into a new squares array
+    squares.push(square);
+  }
+}
+createGrid();

df7a5fb010dfdc3c5e9908d4310b399a7b1deb00
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2022-02-07 12:28:05 +0100

    end game at wall, generate apples

--- js/snake.js
+++ js/snake.js
@@ -4,7 +4,9 @@ const score = document.getElementById("score");
 let squares = [];
 let currentSnake = [0, 1, 2];
 let direction = 1;
-let width = 10;
+const width = 10;
+let appleIndex = 0;
+let timerId = setInterval(move, 1000);
 
 function createGrid() {
   //create 100 of these elements with a for loop
@@ -24,6 +26,20 @@ createGrid();
 currentSnake.forEach((index) => squares[index].classList.add("snake"));
 
 function move() {
+  if (
+    //if hits bottom
+    (currentSnake[0] + width * width >= 100 && direction === width) ||
+    //if hits right
+    (currentSnake[0] % width === width - 1 && direction === 1) ||
+    //if hits left
+    (currentSnake[0] % width === 0 % direction) === -1 ||
+    //if hits top
+    (currentSnake[0] - width < 0 && direction === -width) ||
+    //if snake drives into self
+    squares[currentSnake[0] + direction].classList.contains("snake")
+  )
+    return clearInterval(timerId);
+
   //remove last element from current snake array
   const tail = currentSnake.pop();
   //remove styling from last element
@@ -31,17 +47,31 @@ function move() {
   //add square in direction we are moving
   currentSnake.unshift(currentSnake[0] + direction);
   //add styling so we can see movement
+
+  // deal with snake head getting apple
+  if (currentSnake[0]) {
+    //remove class of apple
+    //grow snake by adding class of snake
+    //grow snake array
+    //generate new apple
+    //add one pt to score
+    //speed up snake
+  }
+
   squares[currentSnake[0]].classList.add("snake");
 }
 move();
 
-let timerId = setInterval(move, 1000);
+function generateApples() {
+  do {
+    appleIndex = Math.floor(Math.random() * squares.length);
+  } while (squares[appleIndex].classList.contains("snake"));
+  squares[appleIndex].classList.add("apple");
+  squares[appleIndex].textContent = "🍎";
+}
+generateApples();
 
-//right
-//up
-//left
-//down
-
+// controls
 function control(e) {
   if (e.keyCode === 39) {
     console.log("right pressed");

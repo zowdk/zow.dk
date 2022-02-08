94238006344bcee1baff218fa1741cc6ccc675e4
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2022-02-08 12:09:24 +0100

    else if to layout styling

--- js/pacman.js
+++ js/pacman.js
@@ -55,11 +55,9 @@ function createBoard() {
 
     if (layout[i] === 0) {
       squares[i].classList.add("pac-dot");
-    }
-    if (layout[i] === 1) {
+    } else if (layout[i] === 1) {
       squares[i].classList.add("wall");
-    }
-    if (layout[i] === 3) {
+    } else if (layout[i] === 3) {
       squares[i].classList.add("power-pellet");
     }
   }

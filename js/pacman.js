28f9a8bff61d3f3f2448ef495dc243fb35a416d6
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2022-02-08 13:46:26 +0100

    pacman respects solid walls

--- js/pacman.js
+++ js/pacman.js
@@ -80,28 +80,40 @@ function control(e) {
   switch (e.keyCode) {
     case 40:
       console.log("pressed down");
-      if (pacmanCurrentIndex + width < width * width) {
+      if (
+        !squares[pacmanCurrentIndex + width].classList.contains("wall") &&
+        pacmanCurrentIndex + width < width * width
+      ) {
         pacmanCurrentIndex += width;
       }
       break;
 
     case 38:
       console.log("pressed up");
-      if (pacmanCurrentIndex - width >= 0) {
+      if (
+        !squares[pacmanCurrentIndex - width].classList.contains("wall") &&
+        pacmanCurrentIndex - width >= 0
+      ) {
         pacmanCurrentIndex -= width;
       }
       break;
 
     case 37:
       console.log("pressed left");
-      if (pacmanCurrentIndex % width !== 0) {
+      if (
+        !squares[pacmanCurrentIndex - 1].classList.contains("wall") &&
+        pacmanCurrentIndex % width !== 0
+      ) {
         pacmanCurrentIndex -= 1;
       }
       break;
 
     case 39:
       console.log("pressed right");
-      if (pacmanCurrentIndex % width < width - 1) {
+      if (
+        !squares[pacmanCurrentIndex + 1].classList.contains("wall") &&
+        pacmanCurrentIndex % width < width - 1
+      ) {
         pacmanCurrentIndex += 1;
       }
 

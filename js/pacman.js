8d1312535abb6e9c767b22cb3629d825837cae6a
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2022-02-08 15:03:13 +0100

    ghost class and styling

--- js/pacman.js
+++ js/pacman.js
@@ -136,3 +136,17 @@ function pacDotEaten() {
     scoreboard.innerHTML = score;
   }
 }
+
+class Ghost {
+  constructor(className, startIndex, speed) {
+    this.className = className;
+    this.startIndex = startIndex;
+    this.speed = speed;
+  }
+}
+ghosts[
+  (new Ghost("blinky", 348, 250),
+  new Ghost("pinky", 376, 400),
+  new Ghost("inky", 351, 300),
+  new Ghost("clyde", 379, 500))
+];

96e3c89e2d7e006cb9fcd198c546395f9f237ef1
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2022-03-15 21:45:47 +0100

    update tallies

--- js/index.js
+++ js/index.js
@@ -1,19 +1,19 @@
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link');
+const navToggle = document.querySelector(".nav-toggle");
+const navLinks = document.querySelectorAll(".nav__link");
 
-navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
-}); 
+navToggle.addEventListener("click", () => {
+  document.body.classList.toggle("nav-open");
+});
 
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open');
-    })
-})
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    document.body.classList.remove("nav-open");
+  });
+});
 
-function sendEventTrackingData(action, category, label){
-    gtag('event', action, {
-        'event_category': category, 
-        'event_label': label
-    });
+function sendEventTrackingData(action, category, label) {
+  gtag("event", action, {
+    event_category: category,
+    event_label: label,
+  });
 }
